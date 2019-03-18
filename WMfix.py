d88b6313cc7cb50d4148284908e701f35c525094
SinTan1729 <35728385+SinTan1729@users.noreply.github.com>
2019-03-18 05:48:29 +0000

    Some fixes

--- WMfix.py
+++ WMfix.py
@@ -2,16 +2,15 @@ import shutil
 
 #firefox-trunk
 
-shutil.copyfile("usr/share/applications/firefox-trunk.desktop","usr/share/applications/firefox-trunk.desktop.bk")
-
 f=open("/usr/share/applications/firefox-trunk.desktop","r")
 contents=f.readlines()
 f.close()
-
-contents.insert(contents.index("[Desktop Action new-window]\n")-1,"StartupWMClass=Nightly\n")
-
-f=open("/usr/share/applications/firefox-trunk.desktop","w")
-f.writelines(contents)
-f.close()
-
-print("fixed firefox-trunk")
+if "StartupWMClass=Nightly\n" not in contents:
+    shutil.copyfile("usr/share/applications/firefox-trunk.desktop","usr/share/applications/firefox-trunk.desktop.bk")
+    contents.insert(contents.index("[Desktop Action new-window]\n")-1,"StartupWMClass=Nightly\n")
+    f=open("/usr/share/applications/firefox-trunk.desktop","w")
+    f.writelines(contents)
+    f.close()
+    print("fixed firefox-trunk")
+else:
+    print("firefox-trunk is okay")
