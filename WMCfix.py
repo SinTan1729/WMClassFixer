72643c819b9b95abf0907e0c29b49fcfd18fd739
SinTan1729 <35728385+SinTan1729@users.noreply.github.com>
2019-03-23 12:31:51 +0000

    Updated script

--- WMCfix.py
+++ WMCfix.py
@@ -6,7 +6,7 @@ f=open("/usr/share/applications/firefox-trunk.desktop","r")
 contents=f.readlines()
 f.close()
 if "StartupWMClass=Nightly\n" not in contents:
-    shutil.copyfile("usr/share/applications/firefox-trunk.desktop","usr/share/applications/firefox-trunk.desktop.bk")
+    shutil.copyfile("/usr/share/applications/firefox-trunk.desktop","/usr/share/applications/firefox-trunk.desktop.bk")
     contents.insert(contents.index("[Desktop Action new-window]\n")-1,"StartupWMClass=Nightly\n")
     f=open("/usr/share/applications/firefox-trunk.desktop","w")
     f.writelines(contents)
